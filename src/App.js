be05ca48bcff116bf933dcf3111cc9aa5f73fdca
luanamarques0 <luanamarques.silv@gmail.com>
2025-07-02 23:03:44 -0300

    feat: adicionar utilitário de notificações com react-toastify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Segment } from 'semantic-ui-react';
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 import Rotas from './Rotas';
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer />
       <Rotas />
 
       <div style={{marginTop: '6%'}}>
